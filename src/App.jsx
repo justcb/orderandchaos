d444b84a3a748b732ee3856597bb3cc5aae9dbd1
justcb <cbrown@medmalfirm.com>
2022-05-26 18:10:34 -0500

    Quiz attempt

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,69 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const personType = "Blue";
 
+const questions = [
+  {
+    questionText: 'I prefer clear rules to follow.',
+    answerOptions: [
+      { answerText: 'True', isCorrect: true },
+      { answerText: 'False', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'The world is better when people can do what they want.',
+    answerOptions: [
+      { answerText: 'True', isCorrect: false },
+      { answerText: 'Flase', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'I would rather be an independent contractor than work in a big corporation.',
+    answerOptions: [
+      { answerText: 'True', isCorrect: false },
+      { answerText: 'False', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'I prefer to select my own goals than have them told to me.',
+    answerOptions: [
+      { answerText: 'True', isCorrect: false },
+      { answerText: 'False', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'I am a ...',
+    answerOptions: [
+      { answerText: 'Shark, I hunt alone.', isCorrect: false },
+      { answerText: 'Wolf, I hunt in packs.', isCorrect: true },
+    ],
+  },
+
+];
+
 const App = () => {
   // State
-  //const [characterType, setCharacterType] = useState(null);
   const [currentAccount, setCurrentAccount] = useState(null);
   const [characterNFT, setCharacterNFT] = useState(null);
-  //const [SplashScreen, setSplashScreen] = useState(null);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+  const scoreType = 0;
+
+
   // Actions
+  const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+      scoreType = 1;
+		}
+	};
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -52,7 +108,32 @@ const renderContent = () => {
 
    * Scenario #1
    */
-  if (!currentAccount) {
+  if (scoreType = 0){ return (
+    <div className='app'>
+    {showScore ? (
+      <div className='score-section'>
+
+        You scored {score}.  That suggests that you should be in Order.  But the choice is yours.  Choose below.
+      </div>
+    ) : (
+      <>
+        <div className='question-section'>
+          <div className='question-count'>
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
+          </div>
+          <div className='question-text'>{questions[currentQuestion].questionText}</div>
+        </div>
+        <div className='answer-section'>
+          {questions[currentQuestion].answerOptions.map((answerOption) => (
+            <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+          ))}
+        </div>
+      </>
+    )}
+  </div>
+);
+  
+} else if (!currentAccount) {
  
       return (
   
