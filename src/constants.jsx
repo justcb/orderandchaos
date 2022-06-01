b6880e2b6b80fed42c81fd0eeb0df9aa4e092ac5
justcb <cbrown@medmalfirm.com>
2022-06-01 08:02:12 -0500

    Clean up of code and files.

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,13 +1,12 @@
-const CONTRACT_ADDRESS = '0x7C1eB732B20c72b9512c8022f14B8763b1eE5796';
+const CONTRACT_ADDRESS = '0xB76BA5706304a1cc3c9352fe5B17365724F0cd23';
 const TOKEN_ADDRESS = '0xb2D7fBcdADcb07a1e5bb4163832E3f53c46Afa6d';
 const transformCharacterData = (characterData) => {
   return {
     name: characterData.name,
     imageURI: characterData.imageURI,
     hp: characterData.hp.toNumber(),
-    //maxHp: characterData.maxHp.toNumber(),
     attackDamage: characterData.attackDamage.toNumber()
   };
 };
 
-export { CONTRACT_ADDRESS, transformCharacterData };
+export { CONTRACT_ADDRESS, transformCharacterData, TOKEN_ADDRESS };
