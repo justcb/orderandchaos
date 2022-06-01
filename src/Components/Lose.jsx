e8f0c9d27062a416af781a7a46500a7914e1ed07
justcb <cbrown@medmalfirm.com>
2022-06-01 16:33:23 -0500

    Upload current contract

--- src/Components/Lose.jsx
+++ src/Components/Lose.jsx
@@ -13,28 +13,58 @@ const Lose = ({ setForm, formData, navigation }) => {
     const { go } = navigation;
     const {next} = navigation;
     const { personType, characterNFT, hasBattled, hasBattled2 } = formData;
+
+    // Splash Screen
     useEffect(() => {
+        // Wait for 15 seconds
         setTimeout(() => {
             setIsLoading(false);
         }, 1500);
     }, []);
-    
+
+
+
+
+    // UseEffects
+    useEffect(() => {
+        const { ethereum } = window;
+
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const gameContract = new ethers.Contract(
+                CONTRACT_ADDRESS,
+                myEpicGame.abi,
+                signer
+            );
+
+            setGameContract(gameContract);
+        } else {
+            console.log('Ethereum object not found');
+        }
+    }, []);
+
+
     return isLoading ?
         <SplashScreen isLoading={isLoading} /> :
         (
             <div className="App">
-            <div className="container">
-                <div className="header-container">
-                    <p className="header gradient-text"></p>
+                <div className="container">
+                    <div className="header-container">
+                        <p className="header gradient-text"></p>
                         <p className="sub-text"></p>
-                            <div className="game-result">
-                                YOU LOSE, the balance has not been maintained... you can now return to the arena or choose a new hacker |
-                            </div>
-                </div>
+                        <div className="arena-container">
+                                        Would you like to revive your player?
+                                    <div className="attack-container">
+                                        <button className="cta-button">
+                                            Revive Player
+                                        </button>
+                                    </div>
+                                </div>
+                         </div>
             </div>
-        </div>
-                
-        )
+            </div>
+    )
 
 }
 
