60d81432449b1303bf30954803db17f81d819b84
Kevin_Gross <kjg2010@live.com>
2022-05-31 19:32:34 -0600

    splash screen attempt

--- src/Components/SplashScreen.jsx
+++ src/Components/SplashScreen.jsx
@@ -7,7 +7,7 @@ const SplashScreen = () => {
     <div className="lds-ring">
       <div></div>
       <div></div>
-        <img src="already_connected_2.svg"></img>
+        <img src="sethmayetlogo.png"></img>
       <div></div>
       <div></div>
     </div>
